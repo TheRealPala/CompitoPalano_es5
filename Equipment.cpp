7ac0edfa59ce49f9c282a2c703b75bffcaeab0d2
Alessandro Palano <apalano23@gmail.com>
2023-06-27 17:21:37 +0200

    FEAT:Added Equipment class

---
+++ Equipment.cpp
@@ -0,0 +1,10 @@
+//
+// Created by ale on 27/06/23.
+//
+
+#include <iostream>
+#include "Equipment.h"
+
+void Equipment::printDescription() {
+    std::cout << "Equipment" << std::endl;
+}
---
+++ Equipment.h
@@ -0,0 +1,17 @@
+//
+// Created by ale on 27/06/23.
+//
+
+#ifndef COMPITOPALANO_ES5_EQUIPMENT_H
+#define COMPITOPALANO_ES5_EQUIPMENT_H
+
+
+#include "Element.h"
+
+class Equipment : public Element{
+    public:
+    void printDescription() override;
+};
+
+
+#endif //COMPITOPALANO_ES5_EQUIPMENT_H
