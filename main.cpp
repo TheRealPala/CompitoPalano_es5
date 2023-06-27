62290c3f439df11e25ca085d0793e2ea603e9391
Alessandro Palano <apalano23@gmail.com>
2023-06-27 18:28:26 +0200

    FEAT:Added main

--- main.cpp
+++ main.cpp
@@ -1,6 +1,40 @@
 #include <iostream>
+#include "Element.h"
+#include "Robot.h"
+#include "Inventory.h"
+#include "Equipment.h"
+#include "Weapon.h"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    Inventory i;
+    i.addElement(new Robot);
+    i.addElement(new Equipment);
+    i.addElement(new Weapon);
+    i.printInventory();
+    std::cout << std::endl;
+    try{
+        i.addElement(new Robot, 1);
+    } catch (std::out_of_range& e) {
+        std::cout << e.what() << "\n" << std::endl;
+    }
+    i.printInventory();
+    std::cout << std::endl;
+    i.deleteElement(1);
+    i.addElement(new Robot, 1);
+    i.printInventory();
+    try{
+        i.deleteElement(5);
+    } catch (std::out_of_range& e) {
+        std::cout << e.what() << "\n" << std::endl;
+    }
+    Inventory i2(3);
+    i2.addElement(new Robot);
+    i2.addElement(new Equipment);
+    i2.addElement(new Weapon);
+    try{
+        i2.addElement(new Robot);
+    } catch (std::out_of_range& e) {
+        std::cout << e.what() << "\n" << std::endl;
+    }
     return 0;
 }
