b446bbf3ae49389cbc36a4b8483e4896cf16ecce
Alessandro Palano <apalano23@gmail.com>
2023-06-27 17:18:54 +0200

    FEAT:Added element class

---
+++ Element.cpp
@@ -0,0 +1,14 @@
+//
+// Created by ale on 27/06/23.
+//
+
+#include <iostream>
+#include "Element.h"
+
+Element::~Element() {
+
+}
+
+void Element::printDescription() {
+    std::cout << "Elemento generico" << std::endl;
+}
---
+++ Element.h
@@ -0,0 +1,16 @@
+//
+// Created by ale on 27/06/23.
+//
+
+#ifndef COMPITOPALANO_ES5_ELEMENT_H
+#define COMPITOPALANO_ES5_ELEMENT_H
+
+
+class Element {
+public:
+    virtual ~Element();
+    virtual void printDescription();
+};
+
+
+#endif //COMPITOPALANO_ES5_ELEMENT_H
